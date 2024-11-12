14487be5f9425e5a7d645ceba091f0b7f0715fa2
MarcoAn778 <marco.duoli2@gmail.com>
2024-11-12 16:13:36 -0300

    Get lists

---
+++ src/main/java/com/devproject/dslist/dto/GameListDTO.java
@@ -0,0 +1,27 @@
+package com.devproject.dslist.dto;
+
+import com.devproject.dslist.entities.GameList;
+
+public class GameListDTO {
+	
+	private Long id;
+	private String name;
+	
+	public GameListDTO() {
+	}
+
+	public GameListDTO(GameList entity) {
+
+		id = entity.getId();
+		name = entity.getName();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+}
